55d29480334ea899a526988e910750cc21281d2b
Raihan Fathurrahman <154324482+raihanfr21@users.noreply.github.com>
2024-06-10 21:47:56 +0700

    Delete EyeTify-ML-Model directory

--- EyeTify-ML-Model/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { upload, uploadAndAnalyzeImage } = require('../controllers/analyzeControllers');
-
-const router = express.Router();
-
-router.post('/upload-analyze', upload.single('image'), uploadAndAnalyzeImage);
-
-module.exports = router;
